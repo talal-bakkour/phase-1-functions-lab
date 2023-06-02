1b1b2ebb654c83aace335494107b4066f7f97094
talal <ta440237@gmail.com>
2023-06-03 02:51:39 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+
+// Function to calculate the distance in blocks from the headquarters
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42);
+  }
+  
+  // Function to calculate the distance in feet from the headquarters
+  function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+  }
+  
+  // Function to calculate the distance traveled in feet
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+  }
+  
+  // Function to calculate the fare price based on the distance
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
+  module.exports = {
+    distanceFromHqInBlocks,
+    distanceFromHqInFeet,
+    distanceTravelledInFeet,
+    calculatesFarePrice
+  };
+  
